add5ca12f58e928f16f9ad4dfe7d19c38a119d65
Mario Adrian Turk <ma_turk@yahoo.com>
2022-07-28 14:05:34 -0300

    Estructura general + datos de ejemplo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,56 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { NgCircleProgressModule } from 'ng-circle-progress';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+
+import { LogoComponent } from './components/header/logo/logo.component';
+
+import { LinksComponent } from './components/header/links/links.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { EducationComponent } from './components/education/education.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoComponent,
+    LinksComponent,
+    BannerComponent,
+    AboutMeComponent,
+    ExperienceComponent,
+    EducationComponent,
+    SkillsComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({
+      backgroundOpacity: 1,
+      backgroundStrokeWidth: 0,
+      backgroundPadding: 0,
+      radius: 100,
+      space: -4,
+      maxPercent: 100,
+      outerStrokeGradient: true,
+      outerStrokeWidth: 16,
+      outerStrokeColor: "blue",
+      outerStrokeGradientStopColor: "green",
+      innerStrokeColor: "green",
+      innerStrokeWidth: 2,
+      titleFontSize: "30",
+      animationDuration: 1500,
+      showSubtitle: false
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
